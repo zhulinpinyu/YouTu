f88ef42d9767f372c8ab8c8e8fce80030348f78f
zhulinpinyu <mulixiang199016@gmail.com>
2016-04-27 16:20:32 +0800

    YT api search try

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 
 const YOUTUBE_API_KEY = "AIzaSyAP46VxAjGPvFhlkfIFFKWtsRipV6yN6XM"
 
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {videos: []}
+
+        YTSearch({key: YOUTUBE_API_KEY, term: "React"}, (videos) => {
+            this.setState({videos: videos})
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(<App />,document.getElementById("app"))
