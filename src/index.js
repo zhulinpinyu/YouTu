e7b16156161424508d27ccdbc5230c569ad7f80d
zhulinpinyu <mulixiang199016@gmail.com>
2016-04-27 09:25:10 +0800

    add component search bar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => <h1>Hi I am Back!</h1>
+import SearchBar from './components/search_bar'
+
+const YOUTUBE_API_KEY = "AIzaSyAP46VxAjGPvFhlkfIFFKWtsRipV6yN6XM"
+
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
+}
 
 ReactDOM.render(<App />,document.getElementById("app"))
