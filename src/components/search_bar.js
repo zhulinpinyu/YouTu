a167c5b34328392f13d01d9cd62f8388a5cf9e93
zhulinpinyu <mulixiang199016@gmail.com>
2016-04-27 15:20:03 +0800

    state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,16 @@
 import React,{Component} from 'react'
 
 export default class SearchBar extends Component {
+    constructor(props){
+        super(props)
+        this.state = {term: ""}
+    }
+
     render(){
         return (
             <div>
-                Search: <input onChange={ e => console.log(e.target.value) }/>
+                Search: <input onChange={ e => this.setState({term: e.target.value}) }/>
+                Value: {this.state.term}
             </div>
         )
     }
